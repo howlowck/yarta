f856c5abc6ae87609793dd34ab591b09e02e2a83
Hao Luo <howlowck@gmail.com>
2020-04-21 14:41:56 -0500

    new types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,8 @@
+import { AnyAction, Reducer as ReduxReducer } from 'redux';
 export interface ReduxActions {
+    [key: string]: AnyAction;
 }
 export declare type ValueOf<T> = T extends {
     [key in keyof T]: infer U;
 } ? U : never;
+export declare type Reducer<S = any> = ReduxReducer<S, ValueOf<ReduxActions>>;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import {AnyAction} from 'redux'
+import { AnyAction, Reducer as ReduxReducer } from 'redux'
 
 export interface ReduxActions {
+  [key: string]: AnyAction
 }
-
 
 export type ValueOf<T> = T extends { [key in keyof T]: infer U ; } ? U : never
 type IsActionFunc<R extends { [key in keyof R]: AnyAction }> = <T extends keyof R>(action: AnyAction, actionType: T) => action is R[T]
 
 const isAction: IsActionFunc<ReduxActions> = (action, actionType): action is ReduxActions[typeof actionType] => action.type === actionType
+
+export type Reducer<S = any> = ReduxReducer<S, ValueOf<ReduxActions>>
